e2346318a34bd6ceaa18d4fb415f2c00bd3432d0
jt <test@example.com>
2017-05-09 19:12:08 +1000

    Completed the decision tree module; percentile in preprocess set to 1

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,7 +26,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# Set up classifier
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+train_st = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-train_st, 3), "s"
 
+# Make some predictions
+test_st = time()
+pred = clf.predict(features_test)
+print "testing time:", round(time()-test_st, 3), "s"
+
+# Grade the accuracy
+acc = accuracy_score(labels_test, pred)
+print acc
 #########################################################
 
 
