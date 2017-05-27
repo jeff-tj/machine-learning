7e9e8eabee39c07fc5b3e904bcc1f6b655b0ef00
jt <test@example.com>
2017-05-27 14:44:25 +0800

    Started identifying POIs using email count, removed outliers. Added project7.ipynb to sketch out thoughts and example pngs.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -7,10 +7,31 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
-### Task 1: Select what features you'll use.
+### Task 1: Select what features you"ll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+# Add a full list of the features available to us
+financial_features = ["salary",
+                      "deferral_payments",
+                      "total_payments",
+                      "loan_advances",
+                      "bonus",
+                      "restricted_stock_deferred",
+                      "deferred_income",
+                      "total_stock_value",
+                      "expenses",
+                      "exercised_stock_options",
+                      "other",
+                      "long_term_incentive",
+                      "restricted_stock",
+                      "director_fees"]
+email_features = ["to_messages",
+                  "email_address",
+                  "from_poi_to_this_person",
+                  "from_messages",
+                  "from_this_person_to_poi",
+                  "shared_receipt_with_poi"]
+features_list = ["poi","salary"] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -28,7 +49,7 @@ labels, features = targetFeatureSplit(data)
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
+### you"ll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
